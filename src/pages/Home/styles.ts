a72281d7d4bab735fd77430088bdd1aa574d9b09
Debson Coelho <debsoncoelh@gmail.com>
2023-08-08 11:04:47 -0300

    create card

--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -2,4 +2,8 @@ import styled from 'styled-components'
 
 export const BackGroundColorHome = styled.div`
   background-color: #282a36;
+  display: flex;
+  flex-direction: column;
+  flex-grow: 1;
+  margin-bottom: auto;
 `
